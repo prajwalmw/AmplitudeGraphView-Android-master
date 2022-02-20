f2105ada48f2152fa4c69c6b672c114bf56a68da
Prajwal <prajwalwaingankar@gmail.com>
2022-02-20 22:00:01 +0530

    Validations added.

--- app/src/main/java/com/suman/voice/voicerecorder/MainActivity.java
+++ app/src/main/java/com/suman/voice/voicerecorder/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,6 @@ import suman.brose.voicerecorder.R;
  */
 
 public class MainActivity extends AppCompatActivity {
-    int i = 0;
     SharedPreferences sf;
     final String preferences = "pref";
     final String saveIt = "saveName";
@@ -39,65 +39,26 @@ public class MainActivity extends AppCompatActivity {
         // getting attached intent data
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_info);
-        final TextView tv1,tv2,tv3,tv4,tv5;
-       /* tv1 = (TextView)findViewById(R.id.textView);
-        tv2 = (TextView)findViewById(R.id.textView2);
-        tv3 = (TextView)findViewById(R.id.textView3);
-        tv4 = (TextView)findViewById(R.id.textView4);
-        tv5 = (TextView)findViewById(R.id.textView5);*/
         final EditText et = (EditText)findViewById(R.id.Name);
+
         final EditText et1 = (EditText)findViewById(R.id.Age);
-      //  final EditText et2 = (EditText)findViewById(R.id.Sex);
+        et1.setFilters(new InputFilter[]{ new InputFilterMinMax("1", "150")}); // Range
+
         final Spinner genderSpinner = findViewById(R.id.spinnerGender);
+
         final EditText et3 = (EditText)findViewById(R.id.Height);
+        et3.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "220")}); // Range
+
         final EditText et4 = (EditText)findViewById(R.id.Weight);
+        et4.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "300")}); // Range
+
         final Spinner smokingSpinner = findViewById(R.id.smokingSpinner);
+
         Button button = (Button)findViewById(R.id.button);
+
         sf = getSharedPreferences(preferences, Context.MODE_PRIVATE);
         Log.d("flowRates","Login"+String.valueOf(sf.getString(saveIt,"")));
 
-//            et.setText(sf.getString(saveIt,""));
-//            et1.setText(sf.getString(saveIt1,""));
-//            et2.setText(sf.getString(saveIt2,""));
-//            et3.setText(sf.getString(saveIt3, ""));
-//            et4.setText(sf.getString(saveIt4,""));
-
-           /* tv1.setText(sf.getString(saveIt, ""));
-            tv2.setText(sf.getString(saveIt1, ""));
-            tv3.setText(sf.getString(saveIt2, ""));
-            tv4.setText(sf.getString(saveIt3, ""));
-            tv5.setText(sf.getString(saveIt4,""));*/
-
-       /* tv1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                i = 0;
-            }
-        });
-        tv2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                i = 1;
-            }
-        });
-        tv3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                i = 2;
-            }
-        });
-        tv4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                i = 3;
-            }
-        });
-        tv5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                i = 4;
-            }
-        });*/
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,6 +74,9 @@ public class MainActivity extends AppCompatActivity {
                     //name
                     if(et.getText().toString().equalsIgnoreCase("")) {
                         et.setError(getString(R.string.this_field_required));
+                        et.requestFocus();
+                        et.setFocusable(true);
+                        et.setFocusableInTouchMode(true);
                     }
                     else {
                         et.setError(null);
@@ -120,7 +84,10 @@ public class MainActivity extends AppCompatActivity {
 
                     //age
                     if(et1.getText().toString().equalsIgnoreCase("")) {
+                        et1.requestFocus();
                         et1.setError(getString(R.string.this_field_required));
+                        et1.setFocusable(true);
+                        et1.setFocusableInTouchMode(true);
                     }
                     else {
                         et1.setError(null);
@@ -128,7 +95,10 @@ public class MainActivity extends AppCompatActivity {
 
                     //height
                     if(et3.getText().toString().equalsIgnoreCase("")) {
+                        et3.requestFocus();
                         et3.setError(getString(R.string.this_field_required));
+                        et3.setFocusable(true);
+                        et3.setFocusableInTouchMode(true);
                     }
                     else {
                         et3.setError(null);
@@ -136,20 +106,21 @@ public class MainActivity extends AppCompatActivity {
 
                     //weight
                     if(et4.getText().toString().equalsIgnoreCase("")) {
+                        et4.requestFocus();
                         et4.setError(getString(R.string.this_field_required));
+                        et4.setFocusable(true);
+                        et4.setFocusableInTouchMode(true);
                     }
                     else {
                         et4.setError(null);
                     }
 
-                    Toast.makeText(MainActivity.this, "Please enter details", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
 
                 final String store = et.getText().toString();
                 final String store1 = et1.getText().toString();
-               // final String store2 = et2.getText().toString();
                 final String store3 = et3.getText().toString();
                 final String store4 = et4.getText().toString();
 
@@ -165,13 +136,11 @@ public class MainActivity extends AppCompatActivity {
 
                 Name = et.getText().toString();
                 Age = et1.getText().toString();
-             //   Sex = store2;
                 Height = et3.getText().toString();
                 Weight = et4.getText().toString();
 
                 Log.d("Login Info","LoginInfo"+Name+" "+Age+" "+Sex);
                 Intent intent = new Intent(getApplicationContext(), MainActivity1.class);
-                //intent.putExtra("selval", selval);
                 intent.putExtra("Name", Name);
                 intent.putExtra("Age",Age);
                 intent.putExtra("Sex",Sex);
